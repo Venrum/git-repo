6f49ea1253655b2fe9bd507e5b8037f6f0963cce
Venrum <21.12.2012@mail.ru>
2016-06-22 21:07:13 +0400

    BMI (ver.2)

--- main5.cpp
+++ main5.cpp
@@ -2,19 +2,18 @@
 #include <conio.h>
 
 int main()
-
 {
 	std::cout << "BMI" << std::endl;
-	double bmi;
-	double height;
-	double weight;
+	double bmi		= 0;
+	double height	= 0;
+	double weight	= 0;
 
 	std::cout << "Enter height in meters: ";
 	std::cin >> height;
 	std::cout << "Enter weight in kg: ";
 	std::cin >> weight;
 
-	bmi = weight / (height*height);
+	bmi = weight / (height * height);
 
 	std::cout << "Your BMI is: " << bmi << std::endl;
 
