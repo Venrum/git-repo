eeb84f93a383755674d22f801d4942522a652a87
Venrum <21.12.2012@mail.ru>
2016-06-24 14:48:01 +0400

    degrees of number "2"(ver.2)

--- main (lesson 5).cpp
+++ main (lesson 5).cpp
@@ -2,11 +2,8 @@
 #include <conio.h>
 #include <cmath>
 
-using std::ios_base;
-
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 	const int n = 2;
 	unsigned long long result = 0;
 	unsigned long long k = 0;
@@ -14,7 +11,7 @@ int main()
 		for (int k(0); k <= 64; k = (k + 1))
 		{
 		result = pow (n,k);
-		std::cout <<  n << " в " << k << " степени" << " это: " << result << "." << std::endl;
+		std::cout << "The " << n << " in " << k << " degrees is: " << result << "." << std::endl;
 		}
 	_getch();
 	return 0;
